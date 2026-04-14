1e3ad810cd4b67320aa5c28b3b29c6da66d4197f
SriAyu06 <sriayuagustini_2428240139@mhs.mdp.ac.id>
2026-04-14 12:01:32 +0700

    komit ke 2

---
+++ localstorage/script.js
@@ -0,0 +1,8 @@
+let barang = document.getElementById('barang');
+let jumlah = document.getElementById('jumlah');
+let keterangan = document.getElementById('keterangan');
+
+function simpan(){
+console.log(barang.value)
+console.log(jumlah.value)
+console.log(keterangan.value)}
